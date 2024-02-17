6897a752136e0e1a7131a9e8ed884f060e0c405e
dzikoysk <dzikoysk@dzikoysk.net>
2024-02-17 23:19:08 +0100

    Release 6.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
     apply(plugin = "signing")
 
     group = "io.javalin.community.routing"
-    version = "6.0.2-SNAPSHOT"
+    version = "6.1.0"
 
     repositories {
         mavenCentral()
@@ -117,12 +117,12 @@ subprojects {
     apply(plugin = "org.jetbrains.kotlin.kapt")
 
     dependencies {
-        val javalin = "6.0.1"
+        val javalin = "6.1.0"
         compileOnly("io.javalin:javalin:$javalin")
         testImplementation("io.javalin:javalin:$javalin")
         testImplementation("io.javalin:javalin-testtools:$javalin")
 
-        val openapi = "6.0.1"
+        val openapi = "6.1.0"
         kaptTest("io.javalin.community.openapi:openapi-annotation-processor:$openapi")
         testImplementation("io.javalin.community.openapi:javalin-openapi-plugin:$openapi")
 
