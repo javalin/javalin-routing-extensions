104cf204174b5c89bc35d4077bf65930947faf48
dzikoysk <dzikoysk@dzikoysk.net>
2024-06-07 22:49:29 +0200

    Mark sources as 6.1.7-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
     apply(plugin = "signing")
 
     group = "io.javalin.community.routing"
-    version = "6.1.6"
+    version = "6.1.7-SNAPSHOT"
 
     repositories {
         mavenCentral()
